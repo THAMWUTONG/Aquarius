3cb3a2d34b6f80cd1aa27a646f98a38bccf4e1ac
thamwutong1717 <thamwutong1717@gmail.com>
2026-07-12 22:39:59 +0800

    Added a really cool background to login

--- frontend/pages/LoginPage.jsx
+++ frontend/pages/LoginPage.jsx
@@ -1,29 +1,49 @@
 import { Link } from "react-router"
+import FloatingLines from "../components/lib/FloatingLines"
 import logo from "../assets/placeholder.png"
 import { FaSignInAlt } from "react-icons/fa"
 
+/**
+ * Displays the login page for user authentication,
+ * this is the default landing page of the website.
+ */
 function LoginPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="max-w-md flex-1 p-8 rounded-xl shadow-lg">
-        <div className="text-center mb-8">
-          <img className="inline w-14 h-14" src={logo} alt="Aquarius Logo"/>
-          <h2 className="text-xl font-bold">Welcome to Aquarius</h2>
-          <p>Your Personalized Learning Assistant</p>
+    <div className="relative min-h-screen">
+      <div style={{ width: '100%', height: '600px', position: 'absolute', zIndex: '-1' }}>
+        <FloatingLines 
+          enabledWaves={["bottom","middle","top"]}
+          lineCount={7}
+          lineDistance={8}
+          bendRadius={8}
+          bendStrength={-2}
+          interactive={false}
+          parallax={false}
+          animationSpeed={0.7}
+          linesGradient={["#32B7F3", "#0d629e", "#094461"]}
+      />
+      </div>
+      <div className="flex justify-center items-center relative z-10 min-h-screen">
+        <div className="max-w-md flex-1 p-8 rounded-xl shadow-lg bg-white">
+          <div className="text-center mb-8">
+            <img className="inline w-14 h-14" src={logo} alt="Aquarius Logo"/>
+            <h2 className="text-xl font-bold">Welcome to Aquarius</h2>
+            <p>Your Personalized Learning Assistant</p>
+          </div>
+        
+          <form className="space-y-4">
+            <div className="flex flex-col gap-2">
+              <label className="text-sm" htmlFor="email">Email Address</label>
+              <input className="p-2 rounded-lg ring ring-gray-300 focus:outline-none focus:ring-2 focus:ring-sky-500" id="email" type="text" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label className="text-sm" htmlFor="password">Password</label>
+              <input className="p-2 rounded-lg ring ring-gray-300 focus:outline-none focus:ring-2 focus:ring-sky-500" id="password" type="password" />
+              <Link className="text-sm underline" to="/forgot-password">Forgot Password?</Link>
+            </div>
+            <button className="flex justify-center items-center gap-2 w-full rounded-lg py-2 font-semibold text-lg text-white bg-sky-500 hover:bg-sky-600 transition-all"type="submit">Sign In <FaSignInAlt/></button>
+          </form>
         </div>
-      
-        <form className="space-y-4">
-          <div className="flex flex-col gap-2">
-            <label className="text-sm" htmlFor="email">Email Address</label>
-            <input className="p-2 rounded-lg ring ring-gray-300 focus:outline-none focus:ring-2 focus:ring-sky-500" id="email" type="text" />
-          </div>
-          <div className="flex flex-col gap-2">
-            <label className="text-sm" htmlFor="password">Password</label>
-            <input className="p-2 rounded-lg ring ring-gray-300 focus:outline-none focus:ring-2 focus:ring-sky-500" id="password" type="password" />
-            <Link className="text-sm underline" to="/forgot-password">Forgot Password?</Link>
-          </div>
-          <button className="flex justify-center items-center gap-2 w-full rounded-lg py-2 font-semibold text-lg text-white bg-sky-500 hover:bg-sky-600 transition-all"type="submit">Sign In <FaSignInAlt/></button>
-        </form>
       </div>
     </div>
   )
